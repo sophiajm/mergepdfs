b1cf10e2207cb5bf790aee27176981aa78d395f3
Sophia M <32445089+sophiajm@users.noreply.github.com>
2020-07-27 15:33:54 -0400

    Upload and comment merge code file

--- merge.py
+++ merge.py
@@ -1,63 +1,69 @@
 ## merge 2 PDF files using PyPDF2
-##cd /Documents/Merge
 
-#import PyPDF2 as ppdf
+## IMPORT NECESSARY PACKAGES ##
 from PyPDF2 import PdfFileMerger
-#from PyPDF2 import PdfFileWriter
 import os
 
-def merge():
-    pdflocation = input('Enter the full folder path to the PDFs, or press enter to use current directory at {}": '.format(os.getcwd()))
+## GET LOCATION FROM USER ##
+pdflocation = input('Enter the full folder path to the PDFs, or press enter to use current directory at {}": '.format(os.getcwd()))
 
-    if len(pdflocation)>0:
-        os.chdir(pdflocation)
-    source_dir = os.getcwd()
-    print("Using directory named ", source_dir)
+## change directory if requested
+if len(pdflocation)>0:
+    os.chdir(pdflocation)
+## get current directory
+source_dir = os.getcwd()
+print("Using directory named ", source_dir)
 
-    outputname = input("Enter the desired output new file name: ")+'.pdf'
-    if outputname in os.listdir(source_dir):
-        newname = input("A file named {} already exists in this directory. Enter a new file name, or press enter to overwrite existing file: ".format(outputname))
-        if len(newname)>0:
-            outputname = newname+'.pdf'
+## GET OUTPUT FILE NAME FROM USER ##
+outputname = input("Enter the desired output new file name: ")+'.pdf'
+## if file already exists
+if outputname in os.listdir(source_dir):
+    newname = input("A file named {} already exists in this directory. Enter a new file name, or press enter to overwrite existing file: ".format(outputname))
+    if len(newname)>0:
+        outputname = newname+'.pdf'
 
-    while True:
-        selectpdf = input("Enter the name of the first pdf, or press enter to merge all pdfs in current directory: ")
-        if len(selectpdf) == 0:
-            break
-        elif selectpdf+'.pdf' not in os.listdir(source_dir):
+## GET FIRST FILE ##
+while True:
+    selectpdf = input("Enter the name of the first pdf, or press enter to merge all pdfs in current directory: ")
+    if len(selectpdf) == 0:
+        break
+    ## check for incorrect file names (could also wrap entire code in try/except)
+    elif selectpdf+'.pdf' not in os.listdir(source_dir):
+        print("File does not exist. Try again, selecting from the files listed below.")
+        print(os.listdir(source_dir))
+        continue
+    ## create list of filenames to merge
+    else:
+        all_pdfs=[selectpdf+'.pdf']
+        break
+
+## CREATE MERGER AND APPEND FILES ##
+merger = PdfFileMerger()
+
+## merge all files in directory
+if len(selectpdf) == 0:
+    for item in os.listdir(source_dir):
+        if item.endswith('pdf'):
+            merger.append(item)
+    print("Merging ", [item for item in os.listdir(source_dir) if item.endswith('pdf')], " as ", outputname)
+
+## merge all files requested by user
+else:
+    while len(selectpdf)>0:
+        selectpdf = input("Enter the name of the next pdf (without file ending), or press enter to merge: ")
+        ## check for incorrect file names
+        if len(selectpdf)>0 and selectpdf+'.pdf' not in os.listdir(source_dir):
             print("File does not exist. Try again, selecting from the files listed below.")
             print(os.listdir(source_dir))
             continue
-        else:
-            all_pdfs=[selectpdf+'.pdf']
-            break
+        if len(selectpdf)>0:
+            all_pdfs.append(selectpdf+'.pdf')
+    for item in all_pdfs:
+        merger.append(item)
+    print("Merging ", all_pdfs, " as ", outputname)
 
-    merger = PdfFileMerger()
+## WRITE OUTPUT FILE AND CLOSE MERGER ##
+merger.write(outputname)
+merger.close()
+print("Complete.")
 
-    if len(selectpdf) == 0:
-        #print(os.listdir(source_dir))
-        for item in os.listdir(source_dir):
-            if item.endswith('pdf'):
-                #print(item, type(item))
-                merger.append(item)
-        print("Merging ", [item for item in os.listdir(source_dir) if item.endswith('pdf')], " as ", outputname)
-
-    else:
-        while len(selectpdf)>0:
-            selectpdf = input("Enter the name of the next pdf (without file ending), or press enter to merge: ")
-            if len(selectpdf)>0 and selectpdf+'.pdf' not in os.listdir(source_dir):
-                print("File does not exist. Try again, selecting from the files listed below.")
-                print(os.listdir(source_dir))
-                continue
-            if len(selectpdf)>0:
-                all_pdfs.append(selectpdf+'.pdf')
-        for item in all_pdfs:
-            #print(item,type(item))
-            merger.append(item)
-        print("Merging ", all_pdfs, " as ", outputname)
-
-
-    merger.write(outputname)
-    merger.close()
-    print("Complete.")
-merge()
